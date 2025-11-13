31bdf022067d86f3235f8b3a985be677ac1b6ca9
VIKAS KUMAR JAISWAL <68907420+vik673@users.noreply.github.com>
2025-11-13 08:29:50 +0530

    Create findTransitionPoint.cpp

---
+++ searching/findTransitionPoint.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int findTransitionPoint(vector<int> arr)
+{
+ int n= arr.size();
+  int lb = 0, ub = n-1;
+  
+  while(lb<=ub)
+  {
+    int mid = (lb+ub)/2;
+    
+    if(arr[mid] ==0)
+      lb = mid+1;
+    else if(arr[mid] ==1)
+    {
+      if(mid ==0 || (mid>0 && arr[mid-1] ==0))
+        return mid;
+      ub = mid-1;
+      
+    }
+  }
+  return -1;
+}
+
+int main() {
+    vector<int> arr = {0,0,0,0,1,1};
+    cout << "Transition point of the element " << findTransitionPoint(arr);
+    return 0;
+}
